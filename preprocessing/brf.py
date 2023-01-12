b7a40efe33d4bae4375aa7fb6825c771fc460ad2
hieubkvn123 <hieubkvn123@gmail.com>
2023-01-12 16:23:35 +0700

    Added GPU acceleration for BRF

--- preprocessing/brf.py
+++ preprocessing/brf.py
@@ -1,5 +1,3 @@
-# SDRF preprocessing, from https://github.com/jctops/understanding-oversquashing
-
 import torch
 import numpy as np
 import networkx as nx
@@ -10,30 +8,46 @@ from torch_geometric.utils import (
 )
 
 class CurvaturePlainGraph():
-    def __init__(self, V, E):
-        self.V = V 
+    def __init__(self, V, E, device=None):
+        self.V = V
         self.E = E
         self.adjacency_matrix = np.full((V,V),np.inf)
+
+        if(device is None):
+          self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        else:
+          self.device = device
+        
         for index in range(V):
             self.adjacency_matrix[index, index] = 0
         for index, edge in enumerate(E):
             self.adjacency_matrix[edge[0], edge[1]] = 1
             self.adjacency_matrix[edge[1], edge[0]] = 1
+        
         # Floyd Warshall
         self.dist = self.adjacency_matrix.copy()
         for k in range(self.V):
             for i in range(self.V):
                 for j in range(self.V):
                     self.dist[i][j] = min(self.dist[i][j], self.dist[i][k] + self.dist[k][j])
-    
+
     def __str__(self):
         return f'The graph contains {self.V} nodes and {len(self.E)} edges {self.E}. '
-    
+
     def visualize(self):
         G = nx.Graph()
         G.add_edges_from(self.E)
         nx.draw_networkx(G)
         plt.show()
+
+    def _to_tensor(self, x):
+        x = torch.Tensor(x).to(self.device)
+        return x
+
+    def _to_numpy(self, x):
+        if(torch.cuda.is_available()):
+            return x.cpu().detach().numpy()
+        return x.detach().numpy()
 
     def _transport_plan_uv(self, u, v, method = 'OTD'):
         u_neighbors = [p for p in range(self.V) if self.adjacency_matrix[u][p] == 1]
@@ -42,19 +56,31 @@ class CurvaturePlainGraph():
         v_deg = len(v_neighbors)
 
         # Instead of using fractions [1/n,...,1/n], [1/m,...,1/m], we use [m,...,m], [n,...,n] and then divides by mn
-        mu = np.full(u_deg, v_deg)
-        mv = np.full(v_deg, u_deg)
+        mu = self._to_tensor(np.full(u_deg, v_deg))
+        mv = self._to_tensor(np.full(v_deg, u_deg))
         sub_indices = np.ix_(u_neighbors, v_neighbors)
-        dist_matrix = self.dist[sub_indices]
+        dist_matrix = self._to_tensor(self.dist[sub_indices])
+        dist_matrix[dist_matrix == np.inf] = 0 # Correct the dist matrix
         if method == 'OTD':
-            optimal_plan = ot.emd(mu, mv, dist_matrix)
+            optimal_plan = self._to_numpy(ot.emd(mu, mv, dist_matrix))
         elif method == 'Sinkhorn':
             optimal_plan = ot.sinkhorn(x, y, d, 1e-1, method='sinkhorn')
         else:
             raise NotImplemented
         optimal_plan = optimal_plan/(u_deg*v_deg)
-        optimal_cost = np.sum(optimal_plan*dist_matrix)
+        optimal_cost = np.sum(optimal_plan*self._to_numpy(dist_matrix))
         return optimal_cost, optimal_plan
+
+    def add_edge(self, i, j):
+        self.adjacency_matrix[i, j] = 1
+        self.adjacency_matrix[j, i] = 1
+        for k in range(self.V):
+            self.dist[i][j] = min(self.dist[i][j], self.dist[i][k] + self.dist[k][j])
+
+    def remove_edge(self, i, j):
+        self.adjacency_matrix[i, j] = 0
+        self.adjacency_matrix[j, i] = 0
+        self.dist[i][j] = np.inf
 
     def curvature_uv(self, u, v, method = 'OTD'):
         optimal_cost, _ = self._transport_plan_uv(u, v, method)
@@ -65,7 +91,7 @@ class CurvaturePlainGraph():
         for edge in self.E:
             edge_curvature_dict[edge] = self.curvature_uv(edge[0], edge[1], method)
         return edge_curvature_dict
-        
+
     def all_curvatures(self, method = 'OTD'):
         C = np.zeros((self.V, self.V))
         for u in range(self.V):
@@ -76,7 +102,7 @@ class CurvaturePlainGraph():
         head = ['C'] + [str(u) for u in range(self.V)]
         print(tabulate(C, floatfmt=".2f", headers=head, tablefmt="presto"))
 
-def preprocess_data(data):
+def _preprocess_data(data, is_undirected=False):
     # Get necessary data information
     N = data.x.shape[0]
     A = np.zeros(shape=(N, N))
@@ -105,6 +131,24 @@ def preprocess_data(data):
 
     return G, N, A, m, C
 
+def _get_neighbors(x, G, is_undirected=False):
+    if is_undirected:
+        x_neighbors = list(G.neighbors(x)) + [x]
+    else:
+        x_neighbors = list(G.successors(x)) + [x]
+    return x_neighbors
+
+def _get_rewire_candidates(G, x_neighbors, y_neighbors):
+    candidates = []
+    for i in x_neighbors:
+        for j in y_neighbors:
+            if (i != j) and (not G.has_edge(i, j)):
+                candidates.append((i, j))
+    return candidates
+
+def _calculate_improvement(C, k, l, x, y):
+  return C
+
 def bfr(
     data,
     loops=10,
@@ -115,33 +159,24 @@ def bfr(
     curvature='bfc'
 ):
     # Preprocess data
-    G, N, A, m, C = preprocess_data(data)
+    G, N, A, m, C = _preprocess_data(data)
 
     # Rewiring begins
     for _ in range(loops):
         # Compute ORC
         can_add = True
-        ollivier_ricci_curvature(A, C=C)
-        
+        graph = CurvaturePlainGraph(N, list(G.edges))
+        C = graph.edge_curvatures(method='OTD')
+
         # Get the neighbors of an edge x-y with
         # minimum curvature (most negative).
-        ix_min = C.argmin()
-        x = ix_min // N
-        y = ix_min % N
-        if is_undirected:
-            x_neighbors = list(G.neighbors(x)) + [x]
-            y_neighbors = list(G.neighbors(y)) + [y]
-        else:
-            x_neighbors = list(G.successors(x)) + [x]
-            y_neighbors = list(G.predecessors(y)) + [y]
-        
+        x, y = min(C, key=C.get)
+        x_neighbors = _get_neighbors(x, G)
+        y_neighbors = _get_neighbors(y, G)
+
         # Get all node pairs from two nodes' neighborhood
         # that are not connected
-        candidates = []
-        for i in x_neighbors:
-            for j in y_neighbors:
-                if (i != j) and (not G.has_edge(i, j)):
-                    candidates.append((i, j))
+        candidates = _get_rewire_candidates(G, x_neighbors, y_neighbors)
 
         # If there are candidates for edge removal
         if len(candidates):
@@ -169,17 +204,4 @@ def bfr(
             if not remove_edges:
                 break
 
-        if remove_edges:
-            ix_max = C.argmax()
-            x = ix_max // N
-            y = ix_max % N
-            if C[x, y] > removal_bound and G.has_edge(x, y):
-                G.remove_edge(x, y)
-                if is_undirected:
-                    A[x, y] = A[y, x] = 0
-                else:
-                    A[x, y] = 0
-            else:
-                if can_add is False:
-                    break
     return from_networkx(G).edge_index, torch.tensor(edge_type)
