d387087933967446ac6cf2663395aa280ab49157
hieubkvn123 <hieubkvn123@gmail.com>
2023-01-16 00:01:30 +0700

    Minor change

--- results/get_missing_experiments.py
+++ results/get_missing_experiments.py
@@ -8,7 +8,7 @@ num_iterations = [1, 2, 3]
 batch_add = [3, 4, 5]
 batch_remove = [1, 2, 3]
 
-cmd = '''
+cmd_template = '''
 python run_graph_classification.py --rewiring brf     
     --layer_type {}   
     --num_trials {}    
@@ -21,18 +21,26 @@ python run_graph_classification.py --rewiring brf
 
 exp_file = 'graph_classification_GCN_brf.csv'
 df = pd.read_csv(exp_file)
-existing_exps = df[['num_iterations', 'brf_batch_add', 'brf_batch_remove']].values
-existing_exps = [set(x) for x in existing_exps]
+existing_exps = df[['dataset', 'num_iterations', 'brf_batch_add', 'brf_batch_remove']].values
+existing_exps = [list(x) for x in existing_exps]
 missing_exps = []
+num_missing = 0
 
 for ds in datasets:
     for it in num_iterations:
         for ba in batch_add:
             for br in batch_remove:
-                if({ds, it, ba, br} not in existing_exps):
-                    cmd = cmd.format(layer_type, seeds, gpu_index, ba, br, it, ds)
+                df_ = df[df['dataset'] == ds]
+                df_ = df_[df_['brf_batch_add'] == ba]
+                df_ = df_[df_['brf_batch_remove'] == br]
+                df_ = df_[df_['num_iterations'] == it]
+                if(len(df_) == 0):
+                    num_missing += 1
+                    cmd = cmd_template.format(layer_type, seeds, gpu_index, ba, br, it, ds)
                     cmd = cmd.strip()
                     cmd = cmd.replace('\n', '')
                     cmd = cmd.replace('\t', ' ')
                     print(cmd)
+
+print(f'# Number of missing experiments : {num_missing}')
 
