cd57beb29ed9649483b1418813471e8017e44a5a
hieubkvn123 <hieubkvn123@gmail.com>
2023-01-15 23:01:37 +0700

    Added graphs

---
+++ sh/run_brf_draft.sh
@@ -0,0 +1,3 @@
+python run_graph_classification.py     --rewiring brf     --layer_type GCN    --num_trials 10    --device cuda:0    --brf_batch_add 5    --brf_batch_remove 3     --num_iterations 1
+python run_graph_classification.py     --rewiring brf     --layer_type GCN    --num_trials 10    --device cuda:0    --brf_batch_add 5    --brf_batch_remove 3     --num_iterations 2
+python run_graph_classification.py     --rewiring brf     --layer_type GCN    --num_trials 10    --device cuda:0    --brf_batch_add 5    --brf_batch_remove 3     --num_iterations 3
