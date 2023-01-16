b851cc806f0875f4aa38574c43c6587048782330
hieubkvn123 <hieubkvn123@gmail.com>
2023-01-16 21:36:54 +0700

    Added graphs

---
+++ sh/run_best_sdrf.sh
@@ -0,0 +1,9 @@
+python run_graph_classification.py --rewiring sdrf_bfc --layer_type GCN --dataset imdb --num_iterations 20
+# python run_graph_classification.py --rewiring sdrf_bfc --layer_type GCN --dataset mutag --num_iterations 5
+# python run_graph_classification.py --rewiring sdrf_bfc --layer_type GCN --dataset enzymes --num_iterations 5
+python run_graph_classification.py --rewiring sdrf_bfc --layer_type GCN --dataset proteins --num_iterations 40
+
+python run_graph_classification.py --rewiring sdrf_bfc --layer_type GIN --dataset imdb --num_iterations 10
+# python run_graph_classification.py --rewiring sdrf_bfc --layer_type GIN --dataset mutag --num_iterations 5
+# python run_graph_classification.py --rewiring sdrf_bfc --layer_type GIN --dataset enzymes --num_iterations 5
+python run_graph_classification.py --rewiring sdrf_bfc --layer_type GIN --dataset proteins --num_iterations 20
