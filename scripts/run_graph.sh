77b0cefc65d52cfe3dda90741f1d06e2fb6846bb
hieubkvn123 <hieubkvn123@gmail.com>
2023-01-25 14:15:50 +0700

    Added run scripts for BORF

---
+++ scripts/run_graph.sh
@@ -0,0 +1,7 @@
+python run_graph_classification.py --rewiring brf --num_trials 100 --num_iterations 1 --brf_batch_add 3 --brf_batch_remove 2 --dataset enzymes
+
+python run_graph_classification.py --rewiring brf --num_trials 100 --num_iterations 1 --brf_batch_add 3 --brf_batch_remove 0 --dataset imdb
+
+python run_graph_classification.py --rewiring brf --num_trials 100 --num_iterations 1 --brf_batch_add 20 --brf_batch_remove 3 --dataset mutag
+
+python run_graph_classification.py --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 4 --brf_batch_remove 1 --dataset proteins
---
+++ scripts/run_node.sh
@@ -0,0 +1,11 @@
+python run_node_classification.py --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 20 --brf_batch_remove 10 --dataset cora 
+
+python run_node_classification.py --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 20 --brf_batch_remove 10 --dataset citeseer 
+
+python run_node_classification.py --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 30 --brf_batch_remove 10 --dataset texas 
+
+python run_node_classification.py --rewiring brf --num_trials 100 --num_iterations 2 --brf_batch_add 20 --brf_batch_remove 30 --dataset cornell 
+
+python run_node_classification.py --rewiring brf --num_trials 100 --num_iterations 2 --brf_batch_add 30 --brf_batch_remove 20 --dataset wisconsin 
+
+python run_node_classification.py --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 20 --brf_batch_remove 20 --dataset chameleon 
