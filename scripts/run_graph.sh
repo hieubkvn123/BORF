6b241cc061e6cd20d1dbc536293b7618615ba570
hieubkvn123 <hieubkvn123@gmail.com>
2023-01-26 02:15:30 +0700

    Minor fix

--- scripts/run_graph.sh
+++ scripts/run_graph.sh
@@ -1,7 +1,17 @@
-python run_graph_classification.py --rewiring brf --num_trials 100 --num_iterations 1 --brf_batch_add 3 --brf_batch_remove 2 --dataset enzymes
+### GCN ###
+python run_graph_classification.py --layer_type GCN --rewiring brf --num_trials 100 --num_iterations 1 --brf_batch_add 3 --brf_batch_remove 2 --dataset enzymes
 
-python run_graph_classification.py --rewiring brf --num_trials 100 --num_iterations 1 --brf_batch_add 3 --brf_batch_remove 0 --dataset imdb
+python run_graph_classification.py --layer_type GCN --rewiring brf --num_trials 100 --num_iterations 1 --brf_batch_add 3 --brf_batch_remove 0 --dataset imdb
 
-python run_graph_classification.py --rewiring brf --num_trials 100 --num_iterations 1 --brf_batch_add 20 --brf_batch_remove 3 --dataset mutag
+python run_graph_classification.py --layer_type GCN --rewiring brf --num_trials 100 --num_iterations 1 --brf_batch_add 20 --brf_batch_remove 3 --dataset mutag
 
-python run_graph_classification.py --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 4 --brf_batch_remove 1 --dataset proteins
+python run_graph_classification.py --layer_type GCN --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 4 --brf_batch_remove 1 --dataset proteins
+
+### GIN ###
+python run_graph_classification.py --layer_type GIN --rewiring brf --num_trials 100 --num_iterations 1 --brf_batch_add 3 --brf_batch_remove 2 --dataset enzymes
+
+python run_graph_classification.py --layer_type GIN --rewiring brf --num_trials 100 --num_iterations 1 --brf_batch_add 3 --brf_batch_remove 0 --dataset imdb
+
+python run_graph_classification.py --layer_type GIN --rewiring brf --num_trials 100 --num_iterations 1 --brf_batch_add 20 --brf_batch_remove 3 --dataset mutag
+
+python run_graph_classification.py --layer_type GIN --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 4 --brf_batch_remove 1 --dataset proteins
--- scripts/run_node.sh
+++ scripts/run_node.sh
@@ -1,11 +1,25 @@
-python run_node_classification.py --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 20 --brf_batch_remove 10 --dataset cora 
+### GCN ###
+python run_node_classification.py --layer_type GCN --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 20 --brf_batch_remove 10 --dataset cora 
 
-python run_node_classification.py --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 20 --brf_batch_remove 10 --dataset citeseer 
+python run_node_classification.py --layer_type GCN --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 20 --brf_batch_remove 10 --dataset citeseer 
 
-python run_node_classification.py --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 30 --brf_batch_remove 10 --dataset texas 
+python run_node_classification.py --layer_type GCN --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 30 --brf_batch_remove 10 --dataset texas 
 
-python run_node_classification.py --rewiring brf --num_trials 100 --num_iterations 2 --brf_batch_add 20 --brf_batch_remove 30 --dataset cornell 
+python run_node_classification.py --layer_type GCN --rewiring brf --num_trials 100 --num_iterations 2 --brf_batch_add 20 --brf_batch_remove 30 --dataset cornell 
 
-python run_node_classification.py --rewiring brf --num_trials 100 --num_iterations 2 --brf_batch_add 30 --brf_batch_remove 20 --dataset wisconsin 
+python run_node_classification.py --layer_type GCN --rewiring brf --num_trials 100 --num_iterations 2 --brf_batch_add 30 --brf_batch_remove 20 --dataset wisconsin 
 
-python run_node_classification.py --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 20 --brf_batch_remove 20 --dataset chameleon 
+python run_node_classification.py --layer_type GCN --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 20 --brf_batch_remove 20 --dataset chameleon 
+
+### GIN ###
+python run_node_classification.py --layer_type GIN --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 20 --brf_batch_remove 10 --dataset cora 
+
+python run_node_classification.py --layer_type GIN --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 20 --brf_batch_remove 10 --dataset citeseer 
+
+python run_node_classification.py --layer_type GIN --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 30 --brf_batch_remove 10 --dataset texas 
+
+python run_node_classification.py --layer_type GIN --rewiring brf --num_trials 100 --num_iterations 2 --brf_batch_add 20 --brf_batch_remove 30 --dataset cornell 
+
+python run_node_classification.py --layer_type GIN --rewiring brf --num_trials 100 --num_iterations 2 --brf_batch_add 30 --brf_batch_remove 20 --dataset wisconsin 
+
+python run_node_classification.py --layer_type GIN --rewiring brf --num_trials 100 --num_iterations 3 --brf_batch_add 20 --brf_batch_remove 20 --dataset chameleon 
