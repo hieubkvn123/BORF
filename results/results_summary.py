e8f32bf9663e78ef2e95db24b1bcc71cef1b1175
hieubkvn123 <hieubkvn123@gmail.com>
2023-02-27 11:39:03 +0700

    Checkpoint

---
+++ results/results_summary.py
@@ -0,0 +1,25 @@
+import glob
+import pandas as pd
+
+model = 'borf'
+keys = ['dataset', 'num_iterations']
+columns = ['avg_accuracy', 'ci', 'brf_batch_add', 'brf_batch_remove']
+if(model != 'brf'):
+    columns = ['avg_accuracy', 'ci']
+columns = keys + columns
+
+for _file in glob.glob(f'node_classification_*{model}*'):
+    print('\nResult for ', _file)
+    df = pd.read_csv(_file)
+
+    if(model == 'none'):
+        df = df[df['num_iterations'] == 10]
+
+    # Find idx of max accuracy
+    idx = df.groupby(['dataset', 'num_iterations']).idxmax(numeric_only=True)['avg_accuracy']
+    result = df.loc[idx][columns]
+    result = result.groupby(['dataset', 'num_iterations']).max()
+    print(result)
+    
+    print(f'Saving to summary_{_file}.xlsx')
+    result.to_excel(f'summary_{_file}.xlsx')
