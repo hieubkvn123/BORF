0c1a0367c0cb4aaef8d5ac95cf6e0b8b4cb0cfa9
hieubkvn123 <hieubkvn123@gmail.com>
2023-03-02 13:33:03 +0700

    Checkpoint

--- get_borf_commands_node.py
+++ get_borf_commands_node.py
@@ -1,31 +1,69 @@
+from argparse import ArgumentParser
 
+def get_commands(dataset, gpu_index, hyperparams):
+    # Unpack hyper-parameters
+    if(isinstance(dataset, list)):
+        datasets = dataset
+    else:
+        datasets = [dataset]
+    layer_types = ['GCN', 'GIN']
+    batch_add = hyperparams['batch_add']
+    batch_remove = hyperparams['batch_remove']
+    num_iterations = hyperparams['num_iterations']
+
+    # Output commands
+    cmd_template = """
+    python run_node_classification.py 
+        --rewiring borf 
+        --layer_type {}
+        --num_trials 100
+        --device cuda:{}
+        --borf_batch_add {}
+        --borf_batch_remove {} 
+        --num_iterations {}
+        --dataset {}
+    """
+
+    for ds in datasets:
+        for layer in layer_types:
+            for ba in batch_add:
+                for br in batch_remove:
+                    for iters in num_iterations:
+                        cmd = cmd_template.format(layer, gpu_index, ba, br, iters, ds)
+                        cmd = cmd.strip()
+                        cmd = cmd.replace('\n', '')
+                        cmd = cmd.replace('\t', ' ')
+
+                        print(cmd)
+
+# Constants
 gpu_index = 0
-layer_types = ["GCN", "GIN"]
-batch_add = [20, 30, 40]
-batch_remove = [10, 20, 30]
-num_iterations = [1, 2, 3] 
-datasets = ['actor'] # 'actor', 'squirrel' , 'ogbn-arxiv'
+datasets = ['actor', 'pubmed', 'squirrel', 'ogbn-arxiv'] 
 
-cmd_template = """
-python run_node_classification.py 
-    --rewiring borf 
-    --layer_type {}
-    --num_trials 100
-    --device cuda:{}
-    --borf_batch_add {}
-    --borf_batch_remove {} 
-    --num_iterations {}
-    --dataset {}
-"""
+# Define multiple grids corresponding to multiple tuning strategies
+hyperparams_grid = {
+    0 : {
+        'batch_add' : [20, 30, 40],
+        'batch_remove' : [10, 20, 30],
+        'num_iterations' : [1, 2, 3]
+    },
+    1 : {
+        'batch_add' : [2, 3, 4],
+        'batch_remove' : [1, 2, 3],
+        'num_iterations' : [5, 10, 15, 20, 30]
+    }
+}
 
-for layer in layer_types:
-    for ba in batch_add:
-        for br in batch_remove:
-            for iters in num_iterations:
-                for ds in datasets:
-                    cmd = cmd_template.format(layer, gpu_index, ba, br, iters, ds)
-                    cmd = cmd.strip()
-                    cmd = cmd.replace('\n', '')
-                    cmd = cmd.replace('\t', ' ')
 
-                    print(cmd)
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--dataset', default=None, required=False)
+    parser.add_argument('--gpu_index', default=0, required=False)
+    args = vars(parser.parse_args())
+
+    if(args['dataset']):
+        datasets = [args['dataset']]
+    gpu_index = args['gpu_index']
+
+    for i, hyperparams in hyperparams_grid.items():
+        get_commands(datasets, gpu_index, hyperparams)
--- results/results_summary.py
+++ results/results_summary.py
@@ -1,25 +1,30 @@
 import glob
 import pandas as pd
 
-model = 'borf'
+model = ['borf', 'sdrf', 'None']
 keys = ['dataset', 'num_iterations']
-columns = ['avg_accuracy', 'ci', 'brf_batch_add', 'brf_batch_remove']
-if(model != 'brf'):
+columns = ['avg_accuracy', 'ci', 'borf_batch_add', 'borf_batch_remove']
+if(model != 'borf'):
     columns = ['avg_accuracy', 'ci']
 columns = keys + columns
 
-for _file in glob.glob(f'node_classification_*{model}*'):
-    print('\nResult for ', _file)
-    df = pd.read_csv(_file)
+if(isinstance(model, list)):
+    models = model
+else:
+    models = [model]
+for m in models:
+    for _file in glob.glob(f'node_classification_*{m}*'):
+        print('\nResult for ', _file)
+        df = pd.read_csv(_file)
 
-    if(model == 'none'):
-        df = df[df['num_iterations'] == 10]
+        if(model == 'none'):
+            df = df[df['num_iterations'] == 10]
 
-    # Find idx of max accuracy
-    idx = df.groupby(['dataset', 'num_iterations']).idxmax(numeric_only=True)['avg_accuracy']
-    result = df.loc[idx][columns]
-    result = result.groupby(['dataset', 'num_iterations']).max()
-    print(result)
-    
-    print(f'Saving to summary_{_file}.xlsx')
-    result.to_excel(f'summary_{_file}.xlsx')
+        # Find idx of max accuracy
+        idx = df.groupby(['dataset', 'num_iterations']).idxmax(numeric_only=True)['avg_accuracy']
+        result = df.loc[idx][columns]
+        result = result.groupby(['dataset', 'num_iterations']).max()
+        print(result)
+        
+        print(f'Saving to summary_{_file}.xlsx')
+        result.to_excel(f'summary_{_file}.xlsx')
