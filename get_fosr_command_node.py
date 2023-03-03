c9b4fdf78d123207d39e2151b9f3f5f931a09ce6
hieubkvn123 <hieubkvn123@gmail.com>
2023-03-03 14:11:02 +0700

    Checkpoint

--- get_fosr_command_node.py
+++ get_fosr_command_node.py
@@ -1,7 +1,7 @@
 gpu_index = 0
 layer_types = ["GCN", "GIN"]
 num_iterations = [25, 50, 75, 100, 125, 150, 175, 200] 
-datasets = ['actor'] 
+datasets = ['pubmed', 'squirrel', 'ogbn-arxiv'] 
 
 cmd_template = """
 python run_node_classification.py 
