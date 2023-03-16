17d2906dbcdd3d7fefc3340124ddc0c8098bc595
hieubkvn123 <hieubkvn123@gmail.com>
2023-03-17 00:04:03 +0700

    Checkpoint

---
+++ preprocessing/borf2.py
@@ -0,0 +1,225 @@
+import os
+import ot
+import copy
+import time
+import glob
+import torch
+import pickle
+import pathlib
+import warnings
+import numpy as np
+import pandas as pd
+import multiprocessing as mp
+import networkx as nx
+from torch_geometric.utils import (
+    to_networkx,
+    from_networkx,
+)
+from torch_geometric.datasets import TUDataset
+from preprocessing.orc import BORFOllivierRicciCurvature
+warnings.filterwarnings("ignore")
+
+def _softmax(a, tau=1):
+    exp_a = np.exp(a * tau)
+    return exp_a / exp_a.sum()
+
+def _preprocess_data(data, is_undirected=False):
+    # Get necessary data information
+    N = data.x.shape[0]
+    m = data.edge_index.shape[1]
+
+    # Compute the adjacency matrix
+    if not "edge_type" in data.keys:
+        edge_type = np.zeros(m, dtype=int)
+    else:
+        edge_type = data.edge_type
+
+    # Convert graph to Networkx
+    G = to_networkx(data)
+    if is_undirected:
+        G = G.to_undirected()
+
+    return G, N, edge_type
+
+def _get_neighbors(x, G, is_undirected=False, is_source=False):
+    if is_undirected:
+        x_neighbors = list(G.neighbors(x)) #+ [x]
+    else:
+        if(is_source):
+          x_neighbors = list(G.successors(x)) #+ [x]
+        else:
+          x_neighbors = list(G.predecessors(x)) #+ [x]
+    return x_neighbors
+
+def _get_rewire_candidates(G, x_neighbors, y_neighbors):
+    candidates = []
+    for i in x_neighbors:
+        for j in y_neighbors:
+            if (i != j) and (not G.has_edge(i, j)):
+                candidates.append((i, j))
+    return candidates
+
+def _calculate_improvement(graph, C, x, y, x_neighbors, y_neighbors, k, l):
+    """
+    Calculate the curvature performance of x -> y when k -> l is added.
+    """
+    graph.add_edge(k, l)
+    old_curvature = C[(x, y)]
+
+    new_curvature, _ = graph.curvature_uv(x, y, u_neighbors=x_neighbors, v_neighbors=y_neighbors)
+    improvement = new_curvature - old_curvature
+    graph.remove_edge(k, l)
+
+    return new_curvature, old_curvature
+
+### Optimized code for rebuttal ###
+def save_graph(G1, G2, graph_idx, fname, iters, added, removed):
+    with open(fname, 'a') as f:
+        # Put number of iterations
+        if(os.path.getsize(fname) <= 0):
+            f.write(f'num_iters={iters}\n')
+
+        # Put graph ID
+        f.write(f'{graph_idx}\n')
+
+        # Put added edges
+        f.write('\tadded\n')
+        for (p, q) in added:
+            if(not G1.has_edge(p, q)):
+                f.write(f'\t\t{p} {q}\n')
+
+        # Put removed edges
+        f.write('\tremoved\n')
+        for (u, v) in removed:
+            if(G1.has_edge(u, v)):
+                f.write(f'\t\t{u} {v}\n')
+
+        # Put final graph edges
+        f.write('\tedges\n')
+        for (k, l) in G2.edges:
+            f.write(f'\t\t{k} {l}\n')
+
+def load_saved_graph(fname):
+    curr_index = None
+    curr_mode = None
+    latest_iters = 0
+    result = {}
+    with open(fname, 'r') as f:
+        lines = f.readlines()
+        latest_iters = int(lines[0].split('=')[1])
+        for line in lines[1:]:
+            if(not line.startswith('\t') and not line.startswith('\t\t')):
+                curr_index = int(line.strip())
+                result[curr_index] = {'added':[], 'removed':[], 'edges':[]}
+            elif(line.startswith('\t') and not line.startswith('\t\t')):
+                curr_mode = line.strip()
+            elif(line.startswith('\t\t')):
+                src, dst = line.strip().split(' ')
+                result[curr_index][curr_mode].append((int(src), int(dst)))
+
+    return result, latest_iters
+
+def load_latest_checkpoint(G, graph_idx, fname):
+    checkpoint, latest_iters = load_saved_graph(fname)
+    if(graph_idx not in checkpoint):
+        return G, 0, [], []
+
+    checkpoint = checkpoint[graph_idx]
+    original_edges = copy.deepcopy(G.edges)
+    
+    for (p, q) in checkpoint['edges']:
+        if(not G.has_edge(p, q)):
+            G.add_edge(p, q)
+
+    for (u, v) in original_edges:
+        if((int(u), int(v)) not in checkpoint['edges']):
+            G.remove_edge(u, v)
+
+    return G, latest_iters, checkpoint['added'], checkpoint['removed']
+
+def borf_optimized(
+    data,
+    loops=10,
+    remove_edges=True,
+    removal_bound=0.5,
+    tau=1,
+    is_undirected=False,
+    batch_add=4,
+    batch_remove=2,
+    device=None,
+    save_dir='rewired_graphs_new',
+    curvature_dir='curvatures_new',
+    dataset_name=None,
+    graph_index=0,
+    debug=False
+):
+    # Check if there is a preprocessed graph
+    dirname = f'{save_dir}/{dataset_name}'
+    curvature_dirname = f'{curvature_dir}/{dataset_name}'
+    pathlib.Path(dirname).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(curvature_dirname).mkdir(parents=True, exist_ok=True)
+
+    # Create graph checkpoint file
+    fname = os.path.join(dirname, f'iters_{loops}_add_{batch_add}_remove_{batch_remove}.txt')
+    curvature_fname = os.path.join(curvature_dirname, f'{graph_index}.pkl')
+    latest_iters = 0
+    added = []
+    removed = []
+
+    # Preprocess data
+    G, N, edge_type = _preprocess_data(data)
+    original_G = copy.deepcopy(G)
+    
+    # Load the latest checkpoint
+    checkpoints = [x for x in glob.glob(os.path.join(dirname, f'iters_*_add_{batch_add}_remove_{batch_remove}.txt')) if x <= fname]
+    checkpoints = sorted(checkpoints)
+    
+    if(len(checkpoints) >= 1):
+        latest_checkpoint = checkpoints[-1]
+        if(str(graph_index) in [x.strip() for x in open(latest_checkpoint, 'r').readlines()]):
+            latest_checkpoint = checkpoints[-1]
+        else:
+            if(len(checkpoints) >= 2):
+                latest_checkpoint = checkpoints[-2]
+        G, latest_iters, added, removed = load_latest_checkpoint(G, graph_index, latest_checkpoint)
+
+    # Rewiring begins
+    for i in range(latest_iters, loops):
+        # Compute ORC
+        if(i == 0 and os.path.exists(curvature_fname)): # Always save the first curvature
+            orc = pickle.load(open(curvature_fname, 'rb'))
+        else:
+            orc = BORFOllivierRicciCurvature(G, device=device, chunk_size=4) 
+            if(i == 0):
+                pickle.dump(orc, open(curvature_fname, 'wb'))
+
+        _C, _PI = orc.edge_curvatures(method='OTD')
+        _C = sorted(_C, key=_C.get)
+
+        # Get top negative and positive curved edges
+        most_pos_edges = _C[-batch_remove:]
+        most_neg_edges = _C[:batch_add]
+
+        # Add edges
+        for (u, v) in most_neg_edges:
+            pi = _PI[(u, v)] 
+            p, q = np.unravel_index(pi.values.argmax(), pi.values.shape)
+            p, q = pi.index[p], pi.columns[q]
+            
+            if(p != q and not G.has_edge(p, q)):
+                G.add_edge(p, q)
+                added.append((p, q))
+
+        # Remove edges
+        for (u, v) in most_pos_edges:
+            if(G.has_edge(u, v)):
+                G.remove_edge(u, v)
+                removed.append((u, v))
+
+    edge_index = from_networkx(G).edge_index
+    edge_type = torch.zeros(size=(len(G.edges),)).type(torch.LongTensor)
+    
+    # Save rewired graph
+    save_graph(original_G, G, graph_index, fname, loops, set(added), set(removed))
+
+    return edge_index, edge_type
---
+++ preprocessing/orc.py
@@ -0,0 +1,134 @@
+import ot
+import torch
+import threading
+import numpy as np
+import pandas as pd
+import networkx as nx
+
+class BORFOllivierRicciCurvature():
+    def __init__(self, G, device=None, is_undirected=False, chunk_size=2):
+        self.G = G
+        self.V = len(G.nodes)
+        self.E = list(G.edges)
+        self.chunk_size = chunk_size
+        self.adjacency_matrix = np.full((self.V,self.V),np.inf)
+
+        if(device is None):
+          self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        else:
+          self.device = device
+        
+        for index in range(self.V):
+            self.adjacency_matrix[index, index] = 0
+        for index, edge in enumerate(self.E):
+            self.adjacency_matrix[edge[0], edge[1]] = 1
+            self.adjacency_matrix[edge[1], edge[0]] = 1
+
+    def __str__(self):
+        return f'The graph contains {self.V} nodes and {len(self.E)} edges {self.E}. '
+
+    def _to_tensor(self, x):
+        x = torch.Tensor(x).to(self.device)
+        return x
+
+    def _to_numpy(self, x):
+        if(torch.cuda.is_available()):
+            return x.cpu().detach().numpy()
+        return x.detach().numpy()
+
+    def _dist_matrix_uv(self, u, v, u_neighbors, v_neighbors):
+        A = self.adjacency_matrix.copy()
+        A[A == np.inf] = 0.0
+        dist = np.zeros(shape=(len(u_neighbors), len(v_neighbors)))
+        for i, _u in enumerate(u_neighbors):
+            for j, _v in enumerate(v_neighbors):
+                # If same node -> distance is zero
+                if(_u == _v):
+                    dist[i, j] = 0
+                    continue
+                
+                # If different nodes
+                # 1. If edge exists -> distance is one
+                if(self.G.has_edge(_u, _v)):
+                    dist[i, j] = 1
+                    continue
+                else:
+                    # 2. If edge does not exists
+                    # 2.1. If have adjacent to same node -> distance is two
+                    # 2.2. If no common node -> distance is three
+                    if(np.sum(A[_u, :] * A[_v, :]) > 0):
+                        dist[i, j] = 2
+                    else:
+                        dist[i, j] = 3      
+        return dist.astype(np.float16)
+
+    def _transport_plan_uv(self, u, v, method = 'OTD'):
+        # Extract N_u, N_v, |N_u|, |N_v|
+        u_neighbors = [p for p in range(self.V) if self.adjacency_matrix[u][p] == 1]
+        v_neighbors = [q for q in range(self.V) if self.adjacency_matrix[v][q] == 1]
+        u_deg = len(u_neighbors)
+        v_deg = len(v_neighbors)
+
+        # Instead of using fractions [1/n,...,1/n], [1/m,...,1/m], we use [m,...,m], [n,...,n] and then divides by mn
+        mu = self._to_tensor(np.full(u_deg, v_deg))
+        mv = self._to_tensor(np.full(v_deg, u_deg))
+        dist_matrix = self._to_tensor(self._dist_matrix_uv(u, v, u_neighbors, v_neighbors))
+        
+        # Update distance matrix
+        if method == 'OTD':
+            optimal_plan = self._to_numpy(ot.emd(mu, mv, dist_matrix))
+        else:
+            raise NotImplemented
+
+        # Find pi, pi * d, sum(pi * d)
+        optimal_plan = optimal_plan / (u_deg * v_deg) # PI
+        optimal_cost = optimal_plan * self._to_numpy(dist_matrix) # optimal_plan = $\pi(p, q)$, optimal_cost = $\pi(p, q) * d(p, q)$
+        optimal_total_cost = np.sum(optimal_cost)
+        optimal_cost = pd.DataFrame(optimal_cost, columns=v_neighbors, index=u_neighbors)
+
+        # Returns sum(pi * d) and pi * d matrix for (u, v)
+        return optimal_total_cost, optimal_cost
+
+    def curvature_uv(self, u, v, method = 'OTD'):
+        optimal_total_cost, optimal_cost = self._transport_plan_uv(u, v, method)
+        return 1 - optimal_total_cost, optimal_cost
+
+    def curvature_uv_wrapper(self, curvature_dict, transport_plan_dict, u, v, method):
+        curvature_dict[(u, v)], transport_plan_dict[(u, v)] = self.curvature_uv(u, v, method)
+
+    def _edge_curvatures_multi_threaded(self, method='OTD'):
+        # Curvatures + transport plans
+        edge_curvature_dict = {}
+        transport_plan_dict = {}
+
+        for chunk_id in range(0, len(self.E), self.chunk_size): 
+            processes = []
+            chunk = self.E[chunk_id:chunk_id + self.chunk_size]
+
+            # Run curvature computation in chunks
+            for (u, v) in chunk:
+                t = threading.Thread(target=lambda : self.curvature_uv_wrapper(edge_curvature_dict, transport_plan_dict, u, v, method))
+                t.start()
+                processes.append(t)
+                
+            # Wait for current chunk to finish
+            for process in processes:
+                process.join()
+              
+        return edge_curvature_dict, transport_plan_dict
+
+    def _edge_curvatures_single_threaded(self, method='OTD'):
+        # Curvatures + transport plans
+        edge_curvature_dict = {}
+        transport_plan_dict = {}
+              
+        for edge in self.E:
+            edge_curvature_dict[edge], transport_plan_dict[edge] = self.curvature_uv(edge[0], edge[1], method)
+        return edge_curvature_dict, transport_plan_dict
+
+    def edge_curvatures(self, method = 'OTD'):
+        if(self.chunk_size > 1):
+            edge_curvature_dict, transport_plan_dict = self._edge_curvatures_multi_threaded(method=method)
+        else:
+            edge_curvature_dict, transport_plan_dict = self._edge_curvatures_single_threaded(method=method)
+        return edge_curvature_dict, transport_plan_dict
